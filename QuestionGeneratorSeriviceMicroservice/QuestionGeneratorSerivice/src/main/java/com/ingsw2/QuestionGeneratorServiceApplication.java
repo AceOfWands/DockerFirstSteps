f40cac271933184be287ebbf041ac1aef6ca7dd1
AndX <andrea0postiglione@gmail.com>
2020-04-07 18:05:17 +0200

    Allowed every origin

--- QuestionGeneratorSeriviceMicroservice/QuestionGeneratorSerivice/src/main/java/com/ingsw2/QuestionGeneratorServiceApplication.java
+++ QuestionGeneratorSeriviceMicroservice/QuestionGeneratorSerivice/src/main/java/com/ingsw2/QuestionGeneratorServiceApplication.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.PostLoad;
@@ -23,6 +26,16 @@ public class QuestionGeneratorServiceApplication extends SpringBootServletInitia
 
 	public static void main(String[] args) {
 	    SpringApplication.run(QuestionGeneratorServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+			}
+		};
 	}
 
 	@PostConstruct
